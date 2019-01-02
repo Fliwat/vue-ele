8635f3bdb02fae54d6bbff1e616a7665a0317048
Fliwat <574562727@qq.com>
2019-01-02 15:02:23 +0800

    tab init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab></tab>
+    </div>
   </div>
 </template>
 
 <script>
 import VHeader from 'components/v-header/v-header'
+import Tab from 'components/tab/tab'
 import { getSeller } from 'api'
 
 export default {
   name: 'app',
   components: {
-    VHeader
+    VHeader,
+    Tab
   },
   data() {
     return {
@@ -32,4 +37,11 @@ export default {
 </script>
 
 <style lang="stylus">
+  #app
+    .tab-wrapper
+      position: fixed
+      top: 136px
+      left: 0
+      right: 0
+      bottom: 0
 </style>
